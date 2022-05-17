2cb63f8a958d830908e153138e6d113be98f1078
Ваше Имя <zhyldyzbek01>
2022-05-17 20:30:18 +0600

    123

---
+++ account/serializers.py
@@ -0,0 +1,113 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from account.models import Category, Post, PostImages, Comment
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=4, write_only=True,
+                                     required=True)
+    password2 = serializers.CharField(min_length=4, write_only=True,
+                                      required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name',
+                  'password', 'password2')
+
+    def validate(self, attrs):
+        password2 = attrs.pop('password2')
+        if password2 != attrs['password']:
+            raise serializers.ValidationError('Пароли не совпали!')
+        return attrs
+
+    @staticmethod
+    def validate_first_name(value):
+        if not value.istitle():
+            raise serializers.ValidationError('Имя должно начинаться с заглавной буквы!!')
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'], )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username',)
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ('password',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImages
+        exclude = ('id',)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(
+        source='owner.username'
+    )
+    images = PostImageSerializer(many=True, read_only=False, required=False)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            'id', 'title', 'body',
+            'owner', 'category', 'preview', 'images', 'comments',
+        )
+
+    def create(self, validated_data):
+        print('validated data: ', validated_data)
+        request = self.context.get('request')
+        print('FILES', request.FILES)
+        images_data = request.FILES
+        created_post = Post.objects.create(**validated_data)
+        print(created_post)
+        print('work', images_data.getlist('images'))
+        images_objects = [PostImages(post=created_post,
+                                     image=image) for image in images_data.getlist('images')]
+        PostImages.objects.bulk_create(images_objects)
+        return created_post
+
+    def is_liked(self, post):
+        user = self.context.get('request').user
+        return user.liked.filter(post=post).exists()
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        CommentSerializer(instance.comments.all(), many=True).data
+        user = self.context.get('request').user
+        if user.is_authenticated:
+            representation['is_liked'] = self.is_liked(instance)
+
+        representation['likes_count'] = instance.likes.count()
+        return representation
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'body', 'owner', 'post')
--- account/views.py
+++ account/views.py
@@ -1,3 +1,181 @@
-from django.shortcuts import render
+from ast import Return
+from django import views
+from rest_auth.views import LogoutView
+from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework import status
+from django.contrib.auth.models import User
 
-# Create your views here.
+from account import serializers
+from account.serializers import PostSerializer
+from account.models import Post, Category, Comment, Likes
+from account.permissions import IsAuthor
+
+
+# TODO permissions to post and comments
+# TODO end comments CRUD
+# TODO Add Likes
+
+
+class StandartPaginationClass(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = serializers.RegisterSerializer
+
+
+class CustomLogoutView(LogoutView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserDetailSerializer
+
+
+# class PostCreateView(generics.CreateAPIView):
+#     serializer_class = serializers.PostSerializer
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+# class PostListView(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = serializers.PostSerializer
+
+# class PostDetailView(generics.RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = serializers.PostSerializer
+
+# class PostUpdateView(generics.UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = serializers.PostSerializer
+
+# class PostDeleteView(generics.DestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = serializers.PostSerializer
+
+
+
+class PostViewSet(ModelViewSet):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    pagination_class = StandartPaginationClass
+    # permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_fields = ('category', 'owner',)
+    search_fields = ('title',)
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    @action(['GET'], detail=True)
+    def comments(self, request, pk):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = serializers.CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+    @action(['POST'], detail=True)
+    def add_to_liked(self, request, pk):
+        post = self.get_object()
+        if request.user.liked.filter(post=post).exists():
+            return Response('Вы уже лайкали этот пост', status=status.HTTP_400_BAD_REQUEST)
+        Likes.objects.create(post=post, user=request.user)
+        return Response('Вы поставили лайк', status=status.HTTP_201_CREATED)
+
+    @action(['POST'], detail=True)
+    def remove_from_liked(self, request, pk):
+        post = self.get_object()
+        if not request.user.liked.filter(post=post).exists():
+            return Response('Вы не лайкали пост', status=status.HTTP_400_BAD_REQUEST)
+        request.user.liked.filter(post=post).delete()
+        Likes.objects.create(post=post, user=request.user)
+        return Response('Ваш лайк удален', status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    # api/v1/posts/1/comments/
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializers_class = serializers.CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAuthor,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+# class PostView(APIView):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True,)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save(owner=request.user)
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
+
+
+# class PostDetailView(APIView):
+#     @staticmethod
+#     def get_object(pk):
+#         try:
+#             return Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             raise Exception("Not found")
+
+#     def get(self, request, pk):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save(owner=request.user)
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors)
+
+#     def delete(self, request, pk):
+#         post = self.get_object(pk)
+#         post.delete()
+#         return Response("Deleted", status=204)
+
+
+class CategoryView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
